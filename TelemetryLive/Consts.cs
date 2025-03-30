9c3cd5bd9188bd158c2f676f4cd8f422bb104381
Tomer <tomer.cohen243@gmail.com>
2025-03-30 16:40:10 +0300

    [feat] added signal r

--- TelemetryLive/Consts.cs
+++ TelemetryLive/Consts.cs
@@ -2,6 +2,8 @@
 {
     class Consts
     {
-
+        public const string STATISTICS_TIMESTAMP_NAME = "timestamp";
+        public const string SGINALR_URL_NAME = "/statistics";
+        public const string UPDATE_STATISTICS_NAME = "updateStatistics";
     }
 }
---
+++ TelemetryLive/SignalR/WebSocketService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using TelemetryLive.Dto;
+
+namespace TelemetryLive.SignalR
+{
+    public class WebSocketService
+    {
+        public IHubContext<StatisticsHub> _websock;
+        public WebSocketService(IHubContext<StatisticsHub> websock) { _websock = websock; }
+
+        public async Task UpdateStatistics(StatisticsRo statisticsRo)
+        {
+            await _websock.Clients.All.SendAsync(Consts.UPDATE_STATISTICS_NAME, statisticsRo);
+        }
+    }
+}
